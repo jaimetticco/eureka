a0776ae73e74a409730944abe84838f183fce525
Jaime Ttito <jaime.tticco.08@gmail.com>
2022-08-09 21:34:50 -0500

    eureka

---
+++ src/test/java/com/idat/eureka/EurekaServerIdatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerIdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
